e92d072a26755eb5fb0e123a6beec079a20d43ef
Alvaro Moure <alvaro.moure1998@gmail.com>
2023-10-29 17:43:21 +0100

    Autocomplete feature for C/C++ even Fortran

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Clone Vundle package manager in the right location for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Symbolic link to set the configuration file to the current user
 ln -s ${PWD}/vimrc ${HOME}/.vimrc
